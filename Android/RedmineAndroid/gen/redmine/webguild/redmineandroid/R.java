df7a1c0414fb8707ae1ec9ad10aa9d6b4cd22286
Дмитрий Сальский <sdiker@ukr.net>
2013-01-17 12:30:09 +0200

    Проверка

--- Android/RedmineAndroid/gen/redmine/webguild/redmineandroid/R.java
+++ Android/RedmineAndroid/gen/redmine/webguild/redmineandroid/R.java
@@ -14,10 +14,15 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int LinearLayout1=0x7f070000;
+        public static final int button1=0x7f070004;
+        public static final int editText1=0x7f070002;
+        public static final int editText2=0x7f070003;
+        public static final int imageView1=0x7f070001;
+        public static final int menu_settings=0x7f070005;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int enter=0x7f030000;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
