05a7126e0815d2b6e38c4cb80bcab6c1d0e19520
N.Nagatkin <jojer7@gmail.com>
2013-01-17 17:22:32 +0200

    Добавил картинки/решил конфликты

--- Android/RedmineAndroid/gen/redmine/webguild/redmineandroid/R.java
+++ Android/RedmineAndroid/gen/redmine/webguild/redmineandroid/R.java
@@ -18,25 +18,26 @@ public final class R {
         public static final int ic_launcher=0x7f020004;
         public static final int mybu=0x7f020005;
         public static final int mypage=0x7f020006;
-        public static final int projects=0x7f020007;
-        public static final int redmine_logo=0x7f020008;
-        public static final int s=0x7f020009;
+        public static final int plus=0x7f020007;
+        public static final int projects=0x7f020008;
+        public static final int redmine_logo=0x7f020009;
+        public static final int s=0x7f02000a;
     }
     public static final class id {
         public static final int Home=0x7f070007;
         public static final int LinearLayout1=0x7f070000;
         public static final int Projects=0x7f070009;
         public static final int TextView01=0x7f07000a;
-        public static final int TextView010=0x7f070012;
-        public static final int TextView02=0x7f070017;
-        public static final int TextView03=0x7f070014;
-        public static final int TextView04=0x7f070013;
-        public static final int TextView05=0x7f070016;
-        public static final int TextView06=0x7f070015;
-        public static final int TextView09=0x7f070010;
+        public static final int TextView010=0x7f070013;
+        public static final int TextView02=0x7f07000d;
+        public static final int TextView03=0x7f070015;
+        public static final int TextView04=0x7f070014;
+        public static final int TextView05=0x7f070017;
+        public static final int TextView06=0x7f070016;
+        public static final int TextView09=0x7f070012;
         public static final int TextView1=0x7f070005;
         public static final int button1=0x7f070004;
-        public static final int button2=0x7f07000f;
+        public static final int button2=0x7f070011;
         public static final int editText1=0x7f070002;
         public static final int editText2=0x7f070003;
         public static final int imageView1=0x7f070001;
@@ -44,18 +45,19 @@ public final class R {
         public static final int listView2=0x7f07000b;
         public static final int menu_settings=0x7f070018;
         public static final int my_page=0x7f070008;
-        public static final int textView1=0x7f07000c;
-        public static final int textView2=0x7f07000d;
-        public static final int textView3=0x7f07000e;
-        public static final int title=0x7f070011;
+        public static final int textView1=0x7f07000e;
+        public static final int textView2=0x7f07000f;
+        public static final int textView3=0x7f070010;
+        public static final int title=0x7f07000c;
     }
     public static final class layout {
         public static final int enter=0x7f030000;
         public static final int home=0x7f030001;
         public static final int mypage=0x7f030002;
-        public static final int project_add_news=0x7f030003;
-        public static final int project_main=0x7f030004;
-        public static final int projectactivity=0x7f030005;
+        public static final int project_activity=0x7f030003;
+        public static final int project_add_news=0x7f030004;
+        public static final int project_main=0x7f030005;
+        public static final int project_news=0x7f030006;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
